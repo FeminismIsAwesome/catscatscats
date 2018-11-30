77073c23229bb939bcc5e6723851dc55e0934739
Ian Norris and Melissa Glickman <pair+ian.norris+melissa.glickman@omadahealth.com>
2018-11-30 10:31:55 -0800

    TOTAL MAJOR REV2
    Co-authored-by: Daniel Carpenter <daniel.carpenter@omadahealth.com>

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -5,6 +5,7 @@ class CatsController < ApplicationController
   def index
     cats_path = Rails.root.join('config', 'cats.csv')
     @cats = CSV.read(cats_path,headers: true)
+    @use_point_buy = true
   end
 
 end
