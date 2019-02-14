91904c1b78a7076902541e86c81113ac67b383e8
Will Hastings <will.hastings@omadahealth.com>
2019-02-13 20:26:25 -0800

    cats v2
    Signed-off-by: Ian Norris <ian.norris@omadahealth.com>

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -5,7 +5,7 @@ class CatsController < ApplicationController
   def index
     cats_path = Rails.root.join('config', 'cats.csv')
     @cats = CSV.read(cats_path,headers: true).sort{|cat| cat["type"] == 'cat' ? 0 : 1}
-    @use_point_buy = true
+    @use_point_buy = false
   end
 
 end
