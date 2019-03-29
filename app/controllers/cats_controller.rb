10a231b7ae230c1e51badc4a91918cb0b954b11a
Ian Norris <ian.norris@omadahealth.com>
2019-03-29 16:49:40 -0700

    MEOW
    Signed-off-by: Juliana Duperron <juliana.duperron@omadahealth.com>

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -4,7 +4,16 @@ class CatsController < ApplicationController
 
   def index
     cats_path = Rails.root.join('config', 'cats.csv')
-    @cats = CSV.read(cats_path,headers: true).sort{|cat| cat["type"] == 'cat' ? 0 : 1}
+    @cats = CSV.read(cats_path,headers: true).sort do
+      | cat |
+      if cat["type"] == 'cat'
+        1
+      elsif cat['type'] == 'starter'
+        0
+      else
+        2
+      end
+    end
     @use_point_buy = false
   end
 
