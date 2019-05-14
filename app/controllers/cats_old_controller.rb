a927b307a33516bd1d77302d71d62fdddafccdc4
Ian Norris <iannorris@BackerKits-MacBook-Pro.local>
2019-05-14 13:15:46 -0700

    meow

---
+++ app/controllers/cats_old_controller.rb
@@ -0,0 +1,20 @@
+require 'csv'
+
+class CatsOldController < ApplicationController
+
+  def index
+    cats_path = Rails.root.join('config', 'cats_old.csv')
+    @cats = CSV.read(cats_path,headers: true).sort do
+    | cat |
+      if cat["type"] == 'cat'
+        1
+      elsif cat['type'] == 'starter'
+        0
+      else
+        cat['description'].present? ? cat['description'].length : 1000
+      end
+    end
+    @use_point_buy = false
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cats, only: [:index]
+  resources :cats_old, only: :index
 end
