5f494481a6b7511d25ccd090401074a9ac83e800
Ian Norris <iannorris@BackerKits-MacBook-Pro.local>
2019-07-23 18:02:29 -0700

    lol

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -4,12 +4,17 @@ class CatsController < ApplicationController
 
   def index
     cats_path = Rails.root.join('config', 'cats.csv')
-    @cats = CSV.read(cats_path,headers: true).sort do
+    cat_ordering = {
+        "award" => 0,
+        "cat" => 2,
+        "starter" => 3,
+        "action" => 1
+    }
+    @cats = CSV.read(cats_path,headers: true).sort_by do
       | cat |
-      if cat["type"] == 'cat'
-        1
-      elsif cat['type'] == 'starter'
-        0
+      cat_order = cat_ordering[cat["type"]]
+      if cat_order.present?
+        cat_order
       else
         cat['description'].present? ? cat['description'].length : 1000
       end
