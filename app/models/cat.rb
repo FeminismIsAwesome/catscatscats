0dbeef4a6f581884f1729b8c03e8978b1fb16400
Ian Norris <ian@backerkit.com>
2019-12-13 18:32:17 -0800

    next wave

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,9 +1,34 @@
 require 'csv'
 
 class Cat
-  def stats(cats_path = Rails.root.join('config', 'cats.csv'))
-    @cats = CSV.read(cats_path,headers: true).group_by do |cat|
+  CATS_PATH = Rails.root.join('config', 'cats.csv')
+  def self.stats
+    @cats = CSV.read(CATS_PATH,headers: true).group_by do |cat|
       cat["type"]
     end
+  end
+
+  def self.cat_distro
+    CSV.read(CATS_PATH, headers: true).select do |cat|
+      cat["type"] == 'cat'
+    end.group_by do |cat|
+      cat["Number-tr"]
+    end
+  end
+
+  def self.bonus_distro(type)
+    CSV.read(CATS_PATH, headers: true).select do |cat|
+      cat["type"] == type
+    end.group_by do |cat|
+      cat["Number-tr"]
+    end.each do |cat, value|
+      puts "#{cat} has #{value.count}"
+    end;nil
+  end
+
+  def self.cat_distro_counts
+    cat_distro.each do |cat, value|
+      puts "#{cat} has #{value.count}"
+    end;nil
   end
 end
