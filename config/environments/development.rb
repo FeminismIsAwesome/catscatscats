5209e26c311add73355ae0dac123e19db7b65d4b
Ian Norris <ian@backerkit.com>
2020-06-25 11:44:29 -0700

    test2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,6 +27,8 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  ENV['API_WS_ROOT'] = 'ws://localhost:3000/cable'
+
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
